92fe2085599014aae6373a55f7b05c5a47dbb8b3
lazward <lazwerd@gmail.com>
2020-02-01 14:25:29 -0500

    Implemented AppleTree movement

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+
+    public GameObject applePrefab ;
+    public float speed = 1f ;
+    public float leftAndRightEdge = 10f ;
+    public float chanceToChangeDirections = 0.1f ;
+    public float secondsBetweenAppleDrops = 1f ;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,29 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = transform.position ;
+        pos.x += speed * Time.deltaTime ;
+        transform.position = pos ;
+
+        if (pos.x < -leftAndRightEdge) {
+
+            speed = Mathf.Abs(speed) ;
+
+        } else if (pos.x > leftAndRightEdge) {
+
+            speed = -Mathf.Abs(speed) ;
+
+        }
         
+    }
+
+    void FixedUpdate() {
+
+        if (Random.value < chanceToChangeDirections) {
+
+            speed *= -1 ;
+
+        }
+
     }
 }
